9a41e053160f6af609af6f0a012d92db6dbbe132
Thomas Steinacher <tom@eggdrop.ch>
2015-04-02 15:51:39 +0200

    Adding Schema

--- flask_sparkle/resources.py
+++ flask_sparkle/resources.py
@@ -1,12 +1,13 @@
 from flask.ext.mongorest.resources import Resource
-from flask_sparkle.documents import Application, Version
+from flask_sparkle import documents, schemas
 
 
 class VersionResource(Resource):
-    document = Version
+    document = documents.Version
 
 class ApplicationResource(Resource):
-    document = Application
+    document = documents.Application
+    schema = schemas.Application
     fields = ['name', 'slug', 'versions', 'date_updated', 'date_created']
     related_resources = {
         'versions': VersionResource
