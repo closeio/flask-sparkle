a4e1b722f8b5bac91a8c30865237d6724b17f1b3
Ilya A. Frolov <ilya+git@close.io>
2016-06-06 12:46:35 +0300

    Add setup.py for easier distribution

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='flask-sparkle',
+    version='0.1',
+    url='http://github.com/closeio/flask-sparkle',
+    license='MIT',
+    description='Flask app that publishes Sparkle update feeds',
+    platforms='any',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+    ],
+    packages=[
+        'flask_sparkle',
+    ],
+)
